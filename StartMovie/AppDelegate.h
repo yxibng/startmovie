d16df1da43dbe9dc73accf2c7cd53746f1be736b
yxibng <yxibng@gmail.com>
2018-12-27 16:41:39 +0800

    Initial Commit

---
+++ StartMovie/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StartMovie
+//
+//  Created by yxibng on 2018/12/27.
+//  Copyright © 2018 yxibng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ StartMovie/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  StartMovie
+//
+//  Created by yxibng on 2018/12/27.
+//  Copyright © 2018 yxibng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
